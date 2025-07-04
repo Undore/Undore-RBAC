104b74e7302daae0c6334c82268add8f100642bc
Undo <hey.ya.dj@gmail.com>
2025-07-04 19:28:43 +0300

    Update routes.py

--- src/routes.py
+++ src/routes.py
@@ -1,13 +1,19 @@
 from typing import Sequence
 from ascender.core.router import RouterRoute
 from controllers.main_controller import MainController
+from shared.rbac.rbac_controller import RBACController
 
-
-routes: Sequence[RouterRoute] = [
+routes: Sequence[RouterRoute | dict] = [
     {
         "path": "/",
         "controller": MainController,
         "tags": ["Main Controller"],
+        "include_in_schema": True
+    },
+    {
+        "path": "/RBAC",
+        "controller": RBACController,
+        "tags": ["UndoreRBAC Controller"],
         "include_in_schema": True
     },
 ]
